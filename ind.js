dbceababea789fb92941cede5deed0ad6e653a94
kumaarpranv <kumaarpranv@gmail.com>
2023-09-16 23:19:25 +0530

    screenshot

--- ind.js
+++ ind.js
@@ -756,8 +756,8 @@ const 获取字体样式 = (字体名,回调)=>{
     setTimeout(_=>如何做爱元素.removeAttribute('data-loading'),2e3);
 });
 
-const 宽 = 1150;
-const 高 = 920;
+const 宽 = 1610;
+const 高 = 1249;
 const 比 = 2;
 
 const 画板 = 新建元素('canvas');
